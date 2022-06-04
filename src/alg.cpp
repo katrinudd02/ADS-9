33733cf9a41b989ae4764c023584cb81fb5708b6
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-06-05 01:11:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,68 @@
 // Copyright 2021 NNTU-CS
+#define _CRT_SECURE_NO_WARNINGS
 #include  <iostream>
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <string>
+#include <cstring>
+#include <sstream>
 
-BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST makeTree(const char* filename) {
+    std::string word, line;
+    BST mTree;
+    char* pch;
+    char* buffer;
+    const char* delim = " ,.-?!;&*#()=+_`~@^$%№/[]:\'\"";
+
+    std::ifstream file(filename);
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return -1;
+    }
+
+    while (getline(file, line)) {
+        buffer = new char[line.size() + 1];
+        std::copy(line.begin(), line.end(), buffer);
+        buffer[line.size()] = '\0';
+        pch = strtok(buffer, delim);
+        while (pch != NULL) {
+            word = pch;
+            word = lower(word);
+            if (checkWord(word)) {
+                mTree.add(word);
+            }
+            pch = strtok(NULL, delim);
+        }
+        delete[] buffer;
+    }
+    file.close();
+    return mTree;
+}
+
+std::string lower(std::string word) {
+    size_t len = word.length();
+    int i;
+    char ch;
+    for (i = 0; i < len; i++) {
+        ch = word[i];
+        if (ch >= 'A' && ch <= 'Z')  word[i] = ch + 32;
+    }
+    return word;
+}
+
+bool checkWord(std::string word) {
+    bool result = true;
+    size_t len = word.length();
+    int i;
+    char ch;
+    for (i = 0; i < len; i++) {
+        ch = word[i];
+        if (ch < 'a' || ch > 'z') {
+            result = false;
+            break;
+        }
+    }
+    return result;
 }
