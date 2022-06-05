b02b093bb64564c1142334110d86c88957cfada6
katrinudd02 <97793336+katrinudd02@users.noreply.github.com>
2022-06-05 03:21:17 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -6,7 +6,7 @@
 #include <algorithm>
 
 class BST {
- public:
+public:
     struct Node {
         std::string word;
         int count;
@@ -14,29 +14,58 @@ class BST {
         Node* right;
     };
 
- private:
+private:
     Node* root;
     Node* addNode(Node*, std::string);
     void printTree(Node*);
     int depthTree(Node*);
     void delTree(Node*);
+    void copyTree(Node*,Node**);
 
- public:
+public:
     BST();
+    BST(const BST&);
     ~BST();
+    BST& operator= (const BST&);
     void add(std::string);
     void print();
     int depth();
     void clear();
     void remove(int);
+    int search(std::string);
 };
 BST::BST() {
     root = nullptr;
 }
 
+BST::BST(const BST& srcTree) {
+    root = nullptr;
+    copyTree(srcTree.root, &root);
+}
+
+BST& BST::operator= (const BST& srcTree) {
+    copyTree(srcTree.root, &root);
+    return *this;
+}
+
 BST::~BST() {
     if (root)
         delTree(root);
+}
+
+void BST::copyTree(Node* src, Node** dst) {
+    if (src == nullptr) {
+        *dst = nullptr;
+        return;
+    }
+    if (*dst == nullptr) {
+        *dst = new Node;
+        (*dst)->count = src->count;
+        (*dst)->word = src->word;
+        (*dst)->left = (*dst)->right = nullptr;
+    }
+    copyTree(src->left,&((*dst)->left));
+    copyTree(src->right, &((*dst)->right));
 }
 
 struct BST::Node* BST::addNode(Node* root, std::string value) {
@@ -45,11 +74,14 @@ struct BST::Node* BST::addNode(Node* root, std::string value) {
         root->count = 1;
         root->word = value;
         root->left = root->right = nullptr;
-    } else if (root->word > value) {
+    }
+    else if (root->word > value) {
         root->left = addNode(root->left, value);
-    } else  if (root->word < value) {
+    }
+    else  if (root->word < value) {
         root->right = addNode(root->right, value);
-    } else {
+    }
+    else {
         root->count++;
     }
     return root;
@@ -74,7 +106,8 @@ void BST::print() {
 void BST::delTree(Node* root) {
     if (root == nullptr) {
         return;
-    } else {
+    }
+    else {
         delTree(root->left);
         delTree(root->right);
         delete root;
@@ -97,5 +130,21 @@ int BST::depthTree(Node* root) {
 
 int BST::depth() {
     return depthTree(root);
+}
+
+int BST::search(std::string word) {
+    Node* p = root;
+    if (!p) return 0;
+    while (p->word != word) {
+        if (p->word > word) p = p->left;
+        else p = p->right;
+        if (p == NULL) break;
+    }
+
+    if (p) {
+        return p->count;
+    } else {
+        return 0;
+    }
 }
 #endif  // INCLUDE_BST_H_
